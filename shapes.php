4fd57ffa1221d3b0c3fbac1cdddb6b5b6a0daf05
petemhdev <petemhdev@gmail.com>
2015-05-21 15:42:22 +0100

    Fixed for Firefox
    Totally reloading the SVG element is necessary for the initial load in
    Firefox or shapes do not display

--- shapes.php
+++ shapes.php
@@ -1,8 +1,8 @@
 		<?php
 
 			//Connect to the database
-			$conn = new PDO('mysql:host=localhost;dbname=svgdb', 'root', '');
 			
+			$conn = new PDO('mysql:host=localhost;dbname=svgdb', 'root', '');
 			try{
 				//If we are in insert mode
 				if(isset($_GET['mode'])){
@@ -17,7 +17,7 @@
 				}
 				else{//Selection mode
 					//Get all data from shapes table
-					
+					echo '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="640" height="480" id="drawing">' . "\n";
 					foreach($conn->query('SELECT * FROM tblshapes') as $row) {
 						//Format shape data into HTML
 						//<rect id="shape1" width="200" height="200" style="fill:rgb(0,0,255)" x="1" y="1" />
@@ -28,9 +28,9 @@
 						. ' height="' . htmlspecialchars($row['height'], ENT_NOQUOTES, 'utf-8') . '"'
 						. ' x="' . htmlspecialchars($row['x'], ENT_NOQUOTES, 'utf-8') . '"'
 						. ' y="' . htmlspecialchars($row['y'], ENT_NOQUOTES, 'utf-8') . '"'
-						. ' style="fill:rgb(' . htmlspecialchars($row['red'], ENT_NOQUOTES, 'utf-8') . ',' . htmlspecialchars($row['green'], ENT_NOQUOTES, 'utf-8') . ',' . htmlspecialchars($row['blue'], ENT_NOQUOTES, 'utf-8') . ')" />' . "\n";
+						. ' style="fill:rgb(' . htmlspecialchars($row['red'], ENT_NOQUOTES, 'utf-8') . ',' . htmlspecialchars($row['green'], ENT_NOQUOTES, 'utf-8') . ',' . htmlspecialchars($row['blue'], ENT_NOQUOTES, 'utf-8') . ')"></rect>' . "\n";
 					}
-					
+					echo '</svg>';
 				}
 			}
 			catch(PDOException $ex){ //If an error occurs output in readable form
