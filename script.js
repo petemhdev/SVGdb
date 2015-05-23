a80cf6de5688777eeab9d4e5805b08fa8686b08b
petemhdev <petemhdev@gmail.com>
2015-05-24 00:18:54 +0100

    Insert function and dynamic menu
    You can now insert new shapes, and the menu is generated from the
    database

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 $( document ).ready(function() {
 	//When any of our range inputs are changed
-  $('input[type=range]').change(function(){
+  $('input[type=range]').mouseup(function(){
+	  
 	  //This gets the index of the selected option from the dropdown list, 
 	  //then selects the object from the list of options, and gets the value
 	   var selectedShape=$("option").eq($("#modify").prop("selectedIndex")).attr("value");
@@ -13,22 +14,25 @@ $( document ).ready(function() {
 		  //Modify the colour string for our shape based on the ranges
 		  //Much more simple to always change all three.
 	
-		  $("#shape" + selectedShape).attr("style","fill:rgb(" + $("#r").prop("value") + "," + $("#g").prop("value") + "," + $("#b").prop("value") + ")");
-		//console.log("fill:rgb(" + $("#r").attr("value") + "," + $("#g").attr("value") + "," + $("#b").attr("value") + ")");
-		  //console.log($("option").eq($("#modify").prop("selectedIndex")).attr("value"));
+		  $("#" + selectedShape).attr("style","fill:rgb(" + $("#r").prop("value") + "," + $("#g").prop("value") + "," + $("#b").prop("value") + ")");
+
 	  }
 	  else{
 			//Whichever range input has changed, we change the property of the same name in the shape
-		 // console.log("#" + selectedShape);
-		  $("#shape" + selectedShape).attr($(this).attr("name"),$(this).prop("value"));
+		  $("#" + selectedShape).attr($(this).attr("name"),$(this).prop("value"));
 	  }
-	  console.log($("#r").attr("value"));
+	 
 	//Save our changes to the database by passing the current values to the php script
-	  		$.get( "shapes.php", { mode: "update",id: selectedShape,
-			width: $("#shape" + selectedShape).attr("width"),
-			height: $("#shape" + selectedShape).attr("height"),
-			x: $("#shape" + selectedShape).attr("x"),
-			y: $("#shape" + selectedShape).attr("y"),
+	  		//Inform user of save process start
+			 $("#message").text("Saving shapes to database...");
+			 //Get key of shape from name
+			 var pKey=selectedShape.match(/[0-9]+/g);
+			 //Pass all changes via GET
+			$.get( "shapes.php", { mode: "update",id: pKey[0],
+			width: $("#" + selectedShape).attr("width"),
+			height: $("#" + selectedShape).attr("height"),
+			x: $("#" + selectedShape).attr("x"),
+			y: $("#" + selectedShape).attr("y"),
 			red: $("#r").prop("value"),
 			green: $("#g").prop("value"),
 			blue: $("#b").prop("value")
@@ -41,40 +45,70 @@ $( document ).ready(function() {
   })
   
   //When we change our shape selection
-  $('select').change(function(){
+  $('select').mouseup(function(){
 	  //Get the newly selected shape
 	  var selectedShape=$("option").eq($("#modify").prop("selectedIndex")).attr("value");
 	  //Adjust the sliders to match the selected shape, I think this can be done in less code
-	  $('input[name=width]').prop("value",$("#shape" + selectedShape).attr("width"));
-	  $('input[name=height]').prop("value",$("#shape" + selectedShape).attr("height"));
-	  $('input[name=x]').prop("value",$("#shape" + selectedShape).attr("x"));
-	  $('input[name=y]').prop("value",$("#shape" + selectedShape).attr("y"));
-	  var style=$("#shape" + selectedShape).attr("style");
+	  $('input[name=width]').prop("value",$("#" + selectedShape).attr("width"));
+	  $('input[name=height]').prop("value",$("#" + selectedShape).attr("height"));
+	  $('input[name=x]').prop("value",$("#" + selectedShape).attr("x"));
+	  $('input[name=y]').prop("value",$("#" + selectedShape).attr("y"));
+	  var style=$("#" + selectedShape).attr("style");
 	  //Extract style information
+
 	  var rgb=style.match(/[0-9]+/g);
 	  $('input[name=red]').prop("value",rgb[0]);
 	  $('input[name=green]').prop("value",rgb[1]);
 	  $('input[name=blue]').prop("value",rgb[2]);
 	  
-	   
 	 
   });
   
   //When the new shape button is pressed
-  $('#newshape').click(function(){
-	  
-	  
+
+  $('a').click(function(e){
+	  e.preventDefault();
+	  //Inform user of insert process start
+			 $("#message").text("Creating new shape in database...");
+			 //Pass all changes via GET
+			$.get( "shapes.php", { mode: "insert",
+			width: 10,
+			height: 10,
+			x: 10,
+			y: 10,
+			red: 254,
+			green: 0,
+			blue: 0
+			} )
+			  .done(function( data ) {
+				  //Inform user of save
+				  $("#message").hide().text("New shape added to database.").fadeIn("200");
+				loadShapes();
+			  });
   });
+  
+  function loadShapes(){
   //Load shapes from MySQL via AJAX
-	
+	//Inform user of AJAX load start
+	 $("#message").text("Loading shapes from database...");
 	  //We get the shapes from a PHP script which searches the database and formats the data
 		  $( "#drawing" ).load( "shapes.php", function() {
 			//Display an update on the screen Google Docs style!
 			 $("#message").hide().text("Shapes loaded from database.").fadeIn("200");
-			//Trigger event to match sliders with shape
-			$("#modify").trigger("change");
-			 
+
+			 //Add shapes to the shape selection menu
+	var menuHTML='';
+
+		for(var i=0;i<$('rect').size();i++){
+			menuHTML=menuHTML + '<option value="' + $('rect').eq(i).attr("id") + '">' + $('rect').eq(i).attr("id") + '</option>'
+		}
+
+		$('#modify').html(menuHTML);
+			//Update sliders
+	$('select').eq(0).trigger('mouseup');
 			});
+
 	
-	  
+  }
+	  loadShapes();
 	});
--- shapes.php
+++ shapes.php
@@ -1,26 +1,39 @@
 		<?php
 
 			//Connect to the database
+			include('dbconfig.php');//Load connection string
 			
-			$conn = new PDO('mysql:host=localhost;dbname=svgdb', 'root', '');
 			try{
-				//If we are in insert mode
+				//If we are in a specified mode (not select)
 				if(isset($_GET['mode'])){
-					//Update the shape as specified by GET data
-					//Using a prepared statement
-					$sql = "UPDATE tblshapes SET width=:width,height=:height,x=:x,y=:y,red=:red,green=:green,blue=:blue WHERE id=:id";
-					$q = $conn->prepare($sql);
-					//Execute query
-					$q->execute(array(':width'=>$_GET['width'],
-									  ':height'=>$_GET['height'],':x'=>$_GET['x'],':y'=>$_GET['y'],
-									  ':red'=>$_GET['red'],':green'=>$_GET['green'],':blue'=>$_GET['blue'],':id'=>$_GET['id']));
+					if($_GET['mode']=="update"){
+						//Update the shape as specified by GET data
+						//Using a prepared statement
+						$sql = "UPDATE tblshapes SET width=:width,height=:height,x=:x,y=:y,red=:red,green=:green,blue=:blue WHERE id=:id";
+						$q = $conn->prepare($sql);
+						//Execute query
+						$q->execute(array(':width'=>$_GET['width'],
+										  ':height'=>$_GET['height'],':x'=>$_GET['x'],':y'=>$_GET['y'],
+										  ':red'=>$_GET['red'],':green'=>$_GET['green'],':blue'=>$_GET['blue'],':id'=>$_GET['id']));
+					}
+					elseif($_GET['mode']=="insert"){
+						//Insert the shape as specified by GET data
+						//Using a prepared statement
+						$sql = "INSERT INTO tblshapes (width,height,x,y,red,green,blue) VALUES (:width,:height,:x,:y,:red,:green,:blue)";
+						$q = $conn->prepare($sql);
+						//Execute query
+						$q->execute(array(':width'=>$_GET['width'],
+										  ':height'=>$_GET['height'],':x'=>$_GET['x'],':y'=>$_GET['y'],
+										  ':red'=>$_GET['red'],':green'=>$_GET['green'],':blue'=>$_GET['blue']));
+										
+					}
 				}
 				else{//Selection mode
 					//Get all data from shapes table
 					echo '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="640" height="480" id="drawing">' . "\n";
 					foreach($conn->query('SELECT * FROM tblshapes') as $row) {
 						//Format shape data into HTML
-						//<rect id="shape1" width="200" height="200" style="fill:rgb(0,0,255)" x="1" y="1" />
+
 						//Escape html special chars for security
 						echo "<rect id=";
 						echo '"shape' . htmlspecialchars($row['id'], ENT_NOQUOTES, 'utf-8') . '"'
